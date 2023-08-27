fb833a006d6cfeb7d9d0d2f84f628a69def9e127
jpssoares <jps.soares@campus.fct.unl.pt>
2023-08-27 23:44:02 +0100

    changes to faq

--- src/faq.py
+++ src/faq.py
@@ -7,10 +7,23 @@ def faq():
         """
 # FAQ
 ## How to use App?
-Just describe the type of property you are looking for:
+Here is an example conversation...
 
-"I am looking for a house in Vilamoura with 3 bedrooms and 3 bathrooms"
+User: "I am looking for a house in Vilamoura with 3 bedrooms and 3 bathrooms"
 
+Bot: "I have found 5 properties that match your criteria. Here is the one of the properties..."
+
+User: "Please describe the property to me"
+
+Bot: "Luxury tourist resort in Vilamoura, Algarve with fully furnished ..."
+
+User: "Please show me some pictures"
+
+Bot: "Here are some photos of the property:(shows pictures)"
+
+User: "Show me the next property"
+
+Bot: "Here is another property..."
 
 ## What tech stack is being uses?
 The ui was built in Python using the Streamlit library. For the conversational part of the app, we are using Meta's Llama 2 through a workflow created on the Clarifai platform. We also created a demo database with information about several properties using MongoDB Atlas, but we intend on integrating a more mainstream API from a well know real estate portal, such as Idealista.
